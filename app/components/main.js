6588f660d7b5c1dde7cf5348f7c0c1ca19b4c2fd
aximon <ochiabutovictor8@gmail.com>
2026-02-21 19:53:53 +0100

    Delete app directory

--- app/components/main.js
+++
@@ -1,6 +0,0 @@
-/*
-* @author ___________
-* This file is responsible for initializing the main components of your application.
-* It should be included in the index.html file in your /app/ folder after the ven.js engine script.
-* You can modify this file to add your own components or initialize any global state for your app.
-*/
--- app/logic/app.js
+++
@@ -1,5 +0,0 @@
-/*
-* @author __________
-* this file holds the logic of your application like the api fetching and other logics that the components will need
-* Modify this file to write your logic
-*/
